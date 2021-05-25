f9cda10eb81c56a68ca43f3408f9315b044ebfef
Rémi <rhaverla@enssat.fr>
2021-05-25 09:00:52 +0200

    Création de la classe Chatbot, relative à la structure REST.

---
+++ chatBots/Chatbot.js
@@ -0,0 +1,28 @@
+class Chatbot {
+
+  constructor(data){  // id, name, personality, state
+    if (undefined != data.id) {
+      this.id = data.id;
+    } else {
+      this.id = parseInt(    Math.floor(Math.random() * Math.floor(100000))     );
+    }
+    if(undefined != data.name) {
+      this.title = data.title;
+    } else {
+      this.title = "";
+    }
+    if(undefined != data.personality) {
+      this.personality = data.personality;
+    } else {
+      this.personality = "";
+    }
+    if(undefined != data.state) {
+      this.state = data.state;
+    } else {
+      this.state = [];
+    }
+  }
+  
+}
+
+module.exports = Chatbot;
