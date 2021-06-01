6accf24f3756be85868f0928756ce4f2a085290d
Ruben <rgalindo@enssat.fr>
2021-06-01 13:53:22 +0200

    Identification
    Debut de l'implémentation de la connexion

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+const utilisateur = require('./utilisateur.json');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -12,6 +15,19 @@ app.get('/', function(req, res) {
 // about page
 app.get('/about', function(req, res) {
     res.render('pages/about');
+});
+
+app.get('/login', function(req, res) {
+    res.render('pages/login');
+});
+
+app.post('/login', function(req, res) {
+  var mail = req.body.user_mail; 
+  var mdp = req.body.user_mdp;
+  const user = utilisateur.find(utilisateur => utilisateur.mail === mail && utilisateur.mdp === mdp)
+  if (typeof user != 'null'){
+	
+	}
 });
 
 app.listen(8080);
